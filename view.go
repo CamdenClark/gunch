786ce21a5e0229ba3a46e85bfff41ee1e7497473
Camden <camdenaws@gmail.com>
2024-05-13 21:06:12 -0700

    Removed unnecessary border and style configurations for files and history in the view.

--- view.go
+++ view.go
@@ -8,43 +8,9 @@ import (
 )
 
 var (
-	leftColumnWidth = 25
 	borderWidth     = 1
 	highlightColor  = lipgloss.AdaptiveColor{Dark: "#7CFC00", Light: "#7CFC00"}
 
-	filesBorder = lipgloss.Border{
-		Top:         "Files────────────────────",
-		TopLeft:     "┌",
-		TopRight:    "┐",
-		Bottom:      "─",
-		Right:       "│",
-		Left:        "│",
-		BottomRight: "┘",
-		BottomLeft:  "└",
-	}
-
-	historyBorder = lipgloss.Border{
-		Top:         "History────────────────────",
-		TopLeft:     "┌",
-		TopRight:    "┐",
-		Bottom:      "─",
-		Right:       "│",
-		Left:        "│",
-		BottomRight: "┘",
-		BottomLeft:  "└",
-	}
-
-	baseFilesStyle = lipgloss.NewStyle().
-			Align(lipgloss.Left).
-			Foreground(lipgloss.Color("#FAFAFA")).
-			Border(filesBorder).
-			Width(leftColumnWidth)
-
-	baseHistoryStyle = lipgloss.NewStyle().
-				Align(lipgloss.Left).
-				Foreground(lipgloss.Color("#FAFAFA")).
-				Border(historyBorder).
-				Width(leftColumnWidth)
 
 	baseChatStyle = lipgloss.NewStyle().
 			Align(lipgloss.Left).
